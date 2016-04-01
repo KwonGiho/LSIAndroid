51c98591cfe83019e45d85772c8d82d3590595a3
kwongiho <shng10503@naver.com>
2016-04-01 15:27:24 +0900

    video get complete

--- app/src/main/java/com/example/kwongyo/lsi/MainActivity.java
+++ app/src/main/java/com/example/kwongyo/lsi/MainActivity.java
@@ -13,7 +13,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -24,10 +28,15 @@ import retrofit2.Retrofit;
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.sendBtn)
     Button sendBtn;
-    @Bind(R.id.imageView)
-    ImageView imageView;
+    @Bind(R.id.videoView)
+    VideoView videoView;
     Bitmap bitmap;
+
+    private MediaController mediaController;
+    private String selectedVideoPath;
     private static int RESULT_LOAD_IMAGE = 1;
+    private static int RESULT_LOAD_VIDEO = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,14 +46,24 @@ public class MainActivity extends AppCompatActivity {
     }
     @OnClick(R.id.sendBtn)
     public void sendBtnClick(View view) {
-        Retrofit movieUploadRetrofit = new Retrofit.Builder().baseUrl("127.0.0.1:8089/MovieUploadServlet").build();
+        //Retrofit movieUploadRetrofit = new Retrofit.Builder().baseUrl("127.0.0.1:8089/MovieUploadServlet").build();
+        Log.d("here","click데스~~1111");
+        Intent i = new Intent(Intent.ACTION_PICK,MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+        i.setType("video/*, images/*");
+        startActivityForResult(i,RESULT_LOAD_VIDEO);
+        Log.d("here","click데스~~");
 
 
     }
-    @OnClick(R.id.imageView)
+    @OnClick(R.id.videoView)
     public void imageViewClick(View view) {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, RESULT_LOAD_IMAGE);
+
+
+        /*Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, RESULT_LOAD_IMAGE);*/
+        /*if( mediaController == null ) {
+            mediaController = new MediaController(this);
+        }*/
     }
     /**
      * 사진 앱범을 열어서 사진을 선택하고 닫으면 이 메소드가 호출 된다.
@@ -71,10 +90,42 @@ public class MainActivity extends AppCompatActivity {
             cursor.close();
 
             bitmap = BitmapFactory.decodeFile(picturePath);
-            imageView.setImageBitmap(bitmap);
+            //imageView.setImageBitmap(bitmap);
 
-        } else {
+        }
+        //http://stackoverflow.com/questions/23994616/android-video-picker-from-gallery
+        else if (requestCode == RESULT_LOAD_VIDEO && resultCode == RESULT_OK && null != data) {
+
+            selectedVideoPath = getPath(data.getData());
+            Toast.makeText(getApplicationContext(), selectedVideoPath, Toast.LENGTH_SHORT).show();
+            try {
+                if(selectedVideoPath == null) {
+                    //Log.e("selected video path = null!","null!!");
+                    finish();
+                } else {
+                    /**
+                     * try to do something there
+                     * selectedVideoPath is path to the selected video
+                     */
+                }
+            }
+            catch(Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
+        else {
             super.onBackPressed();
         }
+    }
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if(cursor!=null) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        else return null;
     }
 }
